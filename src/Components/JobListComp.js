5b3248d4f74cff76d5df68fdc4313d1256faf6b9
Abhishek <abhishek@pop-os.localdomain>
2022-07-05 10:48:13 +0530

    fixing accessibility issues #4

--- src/Components/JobListComp.js
+++ src/Components/JobListComp.js
@@ -30,7 +30,7 @@ function JobListComp({
     }
     return(
     <div role="main" className='flex flex-col gap-4'>
-       <div role="contentinfo" className={`flex flex-col gap-4 py-2 px-4 my-6 bg-teal-50 shadow-lg mx-auto w-96 ${featured && 'border-l-4 border-teal-500 border-solid' } md:flex-row md:w-9/12`}>
+       <div className={`flex flex-col gap-4 py-2 px-4 my-6 bg-teal-50 shadow-lg mx-auto w-96 ${featured && 'border-l-4 border-teal-500 border-solid' } md:flex-row md:w-9/12`}>
          <div className='first--wrapper flex flex-col gap-2'>
             <div className="logo--img w-16 relative mb-4 -mt-8 sm:mt-0">
                 <img src={logo} alt="{job.company}" />
